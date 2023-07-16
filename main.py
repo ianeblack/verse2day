12ab75b54220e31baace05273a59fb2a18637f1f
Iane Black <editedbyiane@gmail.com>
2023-07-15 22:40:18 -0400

    check for wifi connection

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 import textwrap
+import socket
 
-# Make a request to the dailyverses.net website
-response = requests.get('https://dailyverses.net/random-bible-verse/kjv')
+# Check if connected to the internet and run code accordingly
 
-# Create a BeautifulSoup object to parse the HTML content
-soup = BeautifulSoup(response.content, 'html.parser')
+def is_connected_to_internet():
+    try:
+        # Attempt to connect to Google's DNS server (8.8.8.8) on port 53
+        socket.create_connection(("8.8.8.8", 53), timeout=2)
+        return True
+    except OSError:
+        pass
+    return False
 
-# Find the Bible verse element
-verse_element = soup.find(class_='v1')
+def randomVerse():
+    # Make a request to the dailyverses.net website
+    response = requests.get('https://dailyverses.net/random-bible-verse/kjv')
 
-# Find the Bible book element
-book_element = soup.find(class_='vc')
+    # Create a BeautifulSoup object to parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract the verse text
-verse_text = verse_element.text.strip()
+    # Find the Bible verse element
+    verse_element = soup.find(class_='v1')
 
-# Extract the book text
-book = book_element.text.strip()
+    # Find the Bible book element
+    book_element = soup.find(class_='vc')
 
-# text wrap
-verse = textwrap.fill(verse_text, width=30)
+    # Extract the verse text
+    verse_text = verse_element.text.strip()
 
+    # Extract the book text
+    book = book_element.text.strip()
 
-# Print the verse
-print("")
-print(verse)
-print(book)
-print("")
+    # text wrap
+    verse = textwrap.fill(verse_text, width=30)
 
+    # Print the verse
+    print("")
+    print(verse)
+    print(book)
+    print("")
 
+if is_connected_to_internet():
+    randomVerse()
+else:
+    pass
